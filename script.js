edde0cd2d1200f6e5f33847fef2f725c2f97efd9
vamsiram10 <166908201+vamsiram10@users.noreply.github.com>
2024-05-09 14:40:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglemenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".burger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+    /*document is target element */
+}
